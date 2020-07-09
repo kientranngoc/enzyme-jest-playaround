ef2e104d9c4a783c0bb08327ba41f5aae663f03d
Tran Kien <kien@gotitapp.co>
2020-07-09 09:22:05 +0700

    test: Find node from a shallow rendered component

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import App from './App'
+import { shallow } from 'enzyme'
 
 describe('App', () => {
   it('Renders success', () => {
     render(<App />)
+  })
+  const wrapper = shallow(<App />)
+  it('Should contain 1 div element', () => {
+    expect(wrapper.find('div').length).toBe(1)
+  })
+  it('Should contain a div with 1 h1 element', () => {
+    expect(wrapper.find('div').children().length).toBe(1)
   })
 })
