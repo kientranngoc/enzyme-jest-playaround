b8893dfeb8b35fbb92a95c5c544bf7bd67f73755
Tran Kien <kien@gotitapp.co>
2020-07-09 11:11:20 +0700

    test: Use jest snapshot to test App

--- src/App.spec.js
+++ src/App.spec.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import App from './App'
 import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
 
 describe('App', () => {
   it('Renders success', () => {
@@ -13,5 +14,8 @@ describe('App', () => {
   })
   it('Should contain a div with 1 h1 element', () => {
     expect(wrapper.find('div').children().length).toBe(1)
+  })
+  it('Should match with the snapshot', () => {
+    expect(toJson(wrapper)).toMatchSnapshot()
   })
 })
